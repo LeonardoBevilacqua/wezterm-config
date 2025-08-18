016ea34df4061e1644d9a9c7e99cd88da61380fe
Leonardo Almeida Bevilacqua <lbevilacqua@lenovo.com>
2025-08-18 12:59:15 -0300

    including wsl in westerm config

--- windows.lua
+++ windows.lua
@@ -2,8 +2,18 @@ local M = {}
 
 function M.apply_to_config(config)
 	local shell_path = "C:/Users/leonardo.bevilacqua/AppData/Local/Programs/Git/bin/bash.exe"
+	local git_bash = {
+		label = "Git Bash",
+		args = { shell_path, "-l" },
+	}
+	local wsl = {
+		label = "Ubuntu 24.04 (WSL)",
+		args = { "wsl.exe", "-d", "Ubuntu-24.04" },
+	}
 
-	config.default_prog = { shell_path, "-l" }
+	config.launch_menu = { git_bash, wsl }
+
+	config.default_prog = git_bash.args
 	config.set_environment_variables = {
 		SHELL = shell_path,
 	}
